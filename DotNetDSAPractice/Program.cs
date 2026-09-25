3d862fb845a05698db03c0cb0a30de980d40d977
divyanshusoni94 <divyanshusoni@outlook.com>
2026-09-25 11:25:03 +0530

    Two Sum

--- DotNetDSAPractice/Program.cs
+++ DotNetDSAPractice/Program.cs
@@ -1,7 +1,14 @@
-﻿class Program
+﻿//https://github.com/idivyanshusoni
+using DotNetDSAPractice;
+
+class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        //leetcode #1
+        var indices = new TwoSum().Perform(new int[] { 2, 7, 11, 15 }, 9);
+        Console.WriteLine("Indices: " + string.Join(", ", indices));
     }
 }
---
+++ DotNetDSAPractice/TwoSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDSAPractice
+{
+    public class TwoSum
+    {
+        public int[] Perform(int[] nums, int target)
+        {
+            var seen = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (seen.ContainsKey(complement))
+                    return new int[] { seen[complement], i };
+
+                seen[nums[i]] = i;
+            }
+            return Array.Empty<int>();
+        }
+    }
+}
