a197ea6f2151d735d89d4a4e1f8fa3528aa72a4d
divyanshusoni94 <divyanshusoni@outlook.com>
2026-09-26 06:33:52 +0530

    Add Two Numbers

---
+++ DotNetDSAPractice/AddTwoNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDSAPractice
+{
+    public class AddTwoNumbers
+    {
+        public ListNode Perform(ListNode l1, ListNode l2)
+        {
+            ListNode dummy = new ListNode();
+            ListNode current = dummy;
+            int carry = 0;
+
+            while (l1 != null || l2 != null || carry != 0)
+            {
+                int value1 = l1 != null ? l1.val : 0;
+                int value2 = l2 != null ? l2.val : 0;
+
+                int sum = value1 + value2 + carry;
+
+                carry = sum / 10;
+                int digit = sum % 10;
+
+                current.next = new ListNode(digit);
+                current = current.next;
+
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
+            }
+
+            return dummy.next;
+        }
+    }
+}
--- DotNetDSAPractice/Program.cs
+++ DotNetDSAPractice/Program.cs
@@ -7,8 +7,28 @@ class Program
     {
         Console.WriteLine("Hello, World!");
 
-        //leetcode #1
-        var indices = new TwoSum().Perform(new int[] { 2, 7, 11, 15 }, 9);
-        Console.WriteLine("Indices: " + string.Join(", ", indices));
+        ////leetcode #1
+        //var indices = new TwoSum().Perform(new int[] { 2, 7, 11, 15 }, 9);
+        //Console.WriteLine("Indices: " + string.Join(", ", indices));
+
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        var sum = new AddTwoNumbers().Perform(l1, l2);
+        Print(sum);
+    }
+
+    public static void Print(ListNode head)
+    {
+        while (head != null)
+        {
+            Console.Write(head.val);
+
+            if (head.next != null)
+                Console.Write(" --> ");
+
+            head = head.next;
+        }
+
+        Console.WriteLine();
     }
 }
